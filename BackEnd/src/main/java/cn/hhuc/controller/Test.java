b447d97383bebc8de047afa0ff796375622d6a22
tangjiaping <971451619@qq.com>
2020-03-24 22:15:14 +0800

    add trip volume

--- BackEnd/src/main/java/cn/hhuc/controller/Test.java
+++ BackEnd/src/main/java/cn/hhuc/controller/Test.java
@@ -1,4 +1,20 @@
 package cn.hhuc.controller;
 
+import cn.hhuc.bean.TripVolume;
+import cn.hhuc.mapper.ITripVolume;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class Test {
+    @Autowired
+    private ITripVolume tripVolume;
+
+    @RequestMapping("/tripVolume")
+    public List<TripVolume> testTripVolume(){
+        return tripVolume.getAreaTripVolumes();
+    }
 }
--- BackEnd/src/main/java/cn/hhuc/mapper/ITripVolume.java
+++ BackEnd/src/main/java/cn/hhuc/mapper/ITripVolume.java
@@ -1,4 +1,14 @@
 package cn.hhuc.mapper;
 
+import cn.hhuc.bean.TripVolume;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
 public interface ITripVolume {
+    public List<TripVolume> getAreaTripVolumes();
+    public TripVolume getAreaTripVolumeByArea(String area);
 }
